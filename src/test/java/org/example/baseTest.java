e19f45375d6ede3ee6a7a601e82de9f577aaf675
mayank <mayankpayasi8@gmail.com>
2023-05-03 16:57:26 +0530

    ss working

--- src/test/java/org/example/baseTest.java
+++ src/test/java/org/example/baseTest.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
+@Listeners(TestListener.class)
 public class baseTest {
     WebDriver driver;
     CalculatorPage calculatorPage1;
